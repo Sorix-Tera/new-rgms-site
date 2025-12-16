bbb5d01ea416c739a82614848d18799cacab6d58
Sorix-Tera <claptrap92i@gmail.com>
2025-12-16 14:50:25 +0100

    Create .eleventy.js

---
+++ .eleventy.js
@@ -0,0 +1,19 @@
+module.exports = function (eleventyConfig) {
+  // Static assets
+  eleventyConfig.addPassthroughCopy({ "src/css": "css" });
+  eleventyConfig.addPassthroughCopy({ "src/js": "js" });
+  eleventyConfig.addPassthroughCopy({ "src/icons": "icons" });
+  eleventyConfig.addPassthroughCopy({ "src/_redirects": "_redirects" });
+
+  return {
+    dir: {
+      input: "src",
+      output: "dist",
+      includes: "_includes",
+      data: "_data"
+    },
+    templateFormats: ["njk", "html"],
+    htmlTemplateEngine: "njk",
+    markdownTemplateEngine: "njk"
+  };
+};
